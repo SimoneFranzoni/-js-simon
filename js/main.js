9d36ad2059d8ddafae5a34334ec75adbaacb95c5
SimoneFranzoni <simofranzoni99@gmail.com>
2021-11-09 16:59:48 +0100

    3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 let numbers = [];
+let usernums = [];
+const numbernum = 6;
+let contatore = 0;
 
-while (numbers.length < 6){
-    const num = Math.floor(Math.random() * 99) + 1;
-    if(!numbers.includes(num)) numbers.push(num);
+generateNumbers();
 
+checkNumbers();
+
+function generateNumbers(){
+
+    while (numbers.length < numbernum){
+        const num = Math.floor(Math.random() * 99) + 1;
+        if(!numbers.includes(num)) numbers.push(num);
+    
+    }
+    console.log(numbers);
+    
+    for (let i = 0; i < numbers.length; i++){
+        document.querySelector(".number-table").innerHTML += numbers[i] + "   " ;
+    }
+    
 }
-console.log(numbers);
 
-for (i = 0; i < numbers.length; i++){
-    document.querySelector(".number-table").innerHTML += numbers[i] + "  " ;
+function checkNumbers(){
+    
+    for (let i = 0; i < numbers.length; i++){
+        const num = parseInt(prompt("Inserire i numeri uno per volta"));
+        for (let j = 0; j < numbers.length; j++){
+            if(num === numbers[j]){
+                usernums.push(num);
+                contatore++;
+            }
+        }
+    }
 }
 
-
+console.log(usernums);
+console.log(contatore);
